e2a5551454620ba2a43c7b9dff6ba484dc6be218
The Buildbot <buildbot@angular2buch.de>
2022-07-16 22:57:11 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/94ef049499b3fef816cb57525240dc1243f9a503

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BooksModule } from './books/books.module';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BooksModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Subject, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
+
+@Component({
+  selector: 'bm-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+
+  input$ = new Subject<string>();
+  /*BS*/isLoading = false;/*BE*/
+  results: Book[] = [];
+
+  constructor(private bs: BookStoreService) {
+    this.input$.pipe(
+      filter(term => term.length >= 3),
+      debounceTime(500),
+      distinctUntilChanged(),
+      /*BS*/tap(() => this.isLoading = true),/*BE*/
+      switchMap(term => this.bs.getAllSearch(term)),
+      /*BS*/tap(() => this.isLoading = false)/*BE*/
+    )
+    .subscribe(books => {
+      this.results = books;
+    });
+  }
+
+}
